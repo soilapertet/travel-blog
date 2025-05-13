b0bffefec1fdad98d27fc603b5d0d9b093e4e386
Soila Pertet <nicolepertet@gmail.com>
2025-05-13 17:33:29 -0600

    Add a footer section to webpage

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return (
+    <>
+      <footer className='bg-seaGreen absolute bottom-0 w-full p-5'>
+        <h3 className='text-babyPowder text-xl'>&copy; Soila Pertet 2025</h3>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
