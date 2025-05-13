423c63e7f45c54185b16b27d411e269540a2ad13
Soila Pertet <nicolepertet@gmail.com>
2025-05-13 17:21:18 -0600

    Add a header to the page

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import globeLogo from '../assets/images/earth.png'
+
+function Header() {
+  return (
+    <>
+      <header className='bg-seaGreen p-2 m-0 w-full flex flex-row gap-5 justify-center items-center'>
+        <img src={globeLogo} className='w-12' alt="Image of a globe icon" />
+        <h1 className='text-babyPowder text-2xl'>Travel Blog</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        seaGreen : '#3e885b',
+        night : '#00120b',
+        lightGreen : '#aafcb8',
+        babyPowder : '#fffffc'
+      },
+    },
   },
   plugins: [],
 }
